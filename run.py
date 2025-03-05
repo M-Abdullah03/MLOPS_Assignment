bb9cd49e66c30333d429c6ebadaded5450890604
M-Abdullah03 <m18ab03@gmail.com>
2025-03-05 21:27:40 +0500

    Fixed Lint

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app.api.api import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)  # This is a very long comment that will exceed the maximum line length limit set by flake8 and cause the quality check to fail.
+    app.run(debug=True)
